9b5029108f53fd1d93a185659117754615e8c5d1
linuxkay <sccck000@gmail.com>
2019-12-24 14:00:55 +0900

    Link added.

--- sync_time_for_katana.sh
+++ sync_time_for_katana.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/bash
 # This script syncs time for the device behind firewall.
-ssh katana sudo date -s @`( date -u +"%s" )`
+# The following commnad can be found on https://www.commandlinefu.com/commands/view/9153/synchronize-date-and-time-with-a-server-over-ssh
+ssh pi@katana sudo date -s @`( date -u +"%s" )`
