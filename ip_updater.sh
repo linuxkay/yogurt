12e0e1d31ab61fdc449a7c21468e241440857cf4
Linuxkay <sccck000@gmail.com>
2020-06-28 08:12:37 +0900

    README updated

--- ip_updater.sh
+++ ip_updater.sh
@@ -1,5 +1,5 @@
-#!/usr/bin/bash
-# ip_updater for mydns
+#!/usr/bin/sh
+# ip_updater for mydns.jp
 # for IPv4
 wget -O - 'https://username:password@ipv4.mydns.jp/login.html' > /tmp/ip4_update.log
 # for IPv6
