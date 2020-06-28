47d8f78425cf5dc1c888094b439893739aad7889
Linuxkay <sccck000@gmail.com>
2020-06-29 07:54:36 +0900

    fixed log file name

--- ip_updater.sh
+++ ip_updater.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/sh
 # ip_updater for mydns.jp
 # for IPv4
-wget -O - 'https://username:password@ipv4.mydns.jp/login.html' > /tmp/ip4_update.log
+wget -O - 'https://username:password@ipv4.mydns.jp/login.html' > /tmp/IPv4_update.log
 # for IPv6
-wget -O - 'https://username:password@ipv6.mydns.jp/login.html' > /tmp/ip6_update.log
+wget -O - 'https://username:password@ipv6.mydns.jp/login.html' > /tmp/IPv6_update.log
